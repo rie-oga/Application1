98c957e56277da968d68e496e387f3caa1828555
rie <kou.kotetsu@gmail.com>
2020-07-28 00:45:35 +0900

    [Add]4-8

--- routes.rb
+++ routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  resources :blogs
+  
+  get 'top' => 'books#top'
+  root :to => 'books#top'
+
+  get 'books' => 'books#index'
+
+  post 'books' => 'books#create'
+  
+  get 'books/:id/edit' => 'books#edit', as: 'edit'
+
+  get 'books/:id/show' => 'books#show', as: 'show'
+  
+  patch 'books/:id' => 'books#update', as: 'update'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
 end
